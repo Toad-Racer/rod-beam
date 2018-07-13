81dfc9e3426e172e6f5a7a7731ceb592ffe920ab
Toad_Racer <gudak14@gmail.com>
2018-07-13 14:44:15 -0500

    Made log_fns optional

--- output_data.m
+++ output_data.m
@@ -1,9 +1,11 @@
 function output_data(data, const, plot_fn, log_fns)
-    disp('---------------');
-    for msg = keys(log_fns)
-        disp(msg);
-        fn = log_fns(cell2mat(msg));
-        disp(fn(data, const));
+    if log_fns ~= false
+        disp('---------------');
+        for msg = keys(log_fns)
+            disp(msg);
+            fn = log_fns(cell2mat(msg));
+            disp(fn(data, const));
+        end
     end
     
     plot_fn(data, const);
