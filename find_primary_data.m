2487fffc635fb3b24e9547b1d5300483e63a01e9
Toad_Racer <gudak14@gmail.com>
2018-07-12 22:13:47 -0500

    contact implemented

--- find_primary_data.m
+++ find_primary_data.m
@@ -17,15 +17,33 @@ function primary_data = find_primary_data(prev_data, const)
                         a primary_data parameter.
     %}
     f = [prev_data.f1; prev_data.f2];
-    partial_inversion = prev_data.rod_matrix\[f, [1; zeros(length(f)-1, 1)]];
+    I = speye(length(f));
+    partial_inversion = prev_data.rod_matrix\[f, I(:, 1), I(:, const.num_nodes)];
     f_star = partial_inversion(:, 1);
-    I_1 = partial_inversion(:, 2);
+    inv_b = partial_inversion(:, 2);
+    inv_t = partial_inversion(:, 3);
     
-    sigma_db = [1/I_1(1), -1/I_1(1)*f_star(1)]; % Represents sigma_db = 1/a*Wn - 1/a*f_star(1)
-    primary_data = find_beam_data(prev_data, const, sigma_db);
-    sigma_db = sigma_db*[primary_data.w(end); 1]; % Calculate actual scaler value of sigma_db
-    u = f_star + sigma_db*I_1; 
+    sym_sigma_db = [1/inv_b(1), -1/inv_b(1)*f_star(1)]; % Represents sigma_db = 1/a*Wn - 1/a*f_star(1)
+    primary_data = find_beam_data(prev_data, const, -sym_sigma_db);
+    sigma_db = sym_sigma_db*[primary_data.w(end); 1]; % Calculate actual scaler value of sigma_db
+    sigma_dt = 0;
+    u_n = f_star(const.num_nodes) + sigma_db*inv_b(const.num_nodes);
+    
+    if u_n < const.gap % No contact
+        u = f_star + sigma_db*inv_b;
+    else
+        sym_sigma_db = 1/(inv_b(1)-inv_t(1)/inv_t(const.num_nodes)*inv_b(const.num_nodes))* ... 
+            [1, -f_star(1)-inv_t(1)/inv_t(const.num_nodes)*(const.gap-f_star(const.num_nodes))];
+        primary_data = find_beam_data(prev_data, const, -sym_sigma_db);
+        sigma_db = sym_sigma_db*[primary_data.w(end); 1]; % Calculate actual scaler value of sigma_db
+        sigma_dt = 1/inv_t(const.num_nodes)*(const.gap - f_star(const.num_nodes) ...
+            - inv_b(const.num_nodes)*sigma_db);
+        u = f_star + sigma_db*inv_b + sigma_dt*inv_t;
+    end
 
     % split primary data into vectors for u and phi
     primary_data.u = u(1:const.num_nodes);
     primary_data.phi = u(const.num_nodes+1:end);
+    
+    primary_data.sigma_db = sigma_db;
+    primary_data.sigma_dt = sigma_dt;
