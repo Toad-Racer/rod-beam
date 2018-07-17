8a105f87d21fe1922ee89c050f23dc861533457a
Toad_Racer <gudak14@gmail.com>
2018-07-17 17:51:42 -0500

    Added elasticity coefficients

--- coefficient_matrices.m
+++ coefficient_matrices.m
@@ -14,7 +14,8 @@ function mats = coefficient_matrices(const)
                         (but without time-dependent terms).
     %}
     % Rod matrices (each has different dimensions)
-    mats.B11 = 1/const.ht^2*const.Md + (1/2 + const.alpha_d/const.ht)*const.Kd; % (n+1)x(n+1) matrix
+    mats.B11 = 1/const.ht^2*const.Md + ...
+        (const.Ed/2 + const.alpha_d/const.ht)*const.Kd; % (n+1)x(n+1) matrix
     mats.B12 = -const.eta_d/2*const.Sd(:, 2:end); % (n+1)xn matrix
     mats.B21 = const.eta_d/(2*const.ht)*const.Sd(:, 2:end)'; % nx(n+1) matrix   
     % K(r^k)*I is left off because it is time step dependent. See
@@ -23,11 +24,11 @@ function mats = coefficient_matrices(const)
         + const.kappa_th/2*const.Kd(2:end, 2:end); % nxn matrix
     
     % Beam matrices (all nxn)
-    mats.B33 = (1/const.ht^2+const.alpha_b/const.ht+1)*const.Mb ...
-        + (1+const.alpha_b/const.ht)*const.Kb;
-    mats.B34 = -(1+const.alpha_b/const.ht)*const.Sb';
-    mats.B43 = -(1+const.alpha_b/const.ht)*const.Sb;
+    mats.B33 = (1/const.ht^2+const.alpha_b/const.ht+const.G)*const.Mb ...
+        + (const.Eb+const.alpha_b/const.ht)*const.Kb;
+    mats.B34 = -(const.G+const.alpha_b/const.ht)*const.Sb';
+    mats.B43 = -(const.G+const.alpha_b/const.ht)*const.Sb;
     % N^(k+1) is left off because it is time step dependent. See
     % find_beam_data.m for its addition.
     mats.B44 = (1/const.ht^2)*const.Mb ... 
-        + (1+const.alpha_b/const.ht)*const.Kb;
+        + (const.G+const.alpha_b/const.ht)*const.Kb;
