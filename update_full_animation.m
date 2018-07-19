4af50c75d9fbac3868efe9dbb75b902401b65641
Toad_Racer <gudak14@gmail.com>
2018-07-19 09:03:26 -0500

    Stopped displaying filtered rod points (they were at y=0)

--- update_full_animation.m
+++ update_full_animation.m
@@ -7,7 +7,8 @@ function update_full_animation(lines, data, const)
     clearpoints(lines.beam);
     clearpoints(lines.rod_b);
     addpoints(lines.beam, x, full(data.w));
-    addpoints(lines.rod_b, 1+(0*y), rod_animation_mask(const).*rod_pos);
+    filtered_rod = nonzeros(rod_animation_mask(const).*rod_pos);
+    addpoints(lines.rod_b, ones(1, length(filtered_rod)), filtered_rod);
     
     % Beam's velocity
     clearpoints(lines.beam_velo);
