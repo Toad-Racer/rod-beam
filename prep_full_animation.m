d1ed412bdbc27c3f9ad2887821c5f1720b81ad14
Toad_Racer <gudak14@gmail.com>
2018-07-19 09:10:34 -0500

    Changed rigid obstacle representation to a thin line (was diamonds)

--- prep_full_animation.m
+++ prep_full_animation.m
@@ -6,13 +6,12 @@ function lines = prep_full_animation(initial_data, const)
     % Rod and Beam
     subplot(3, 3, [1 2 4 5 7 8]);
     title('Rod and beam');
-    axis([0, 1.1, -0.1, 1.003*const.Ly+const.gap]);
+    axis([0, 1.1, -0.2, 1.03*const.Ly+const.gap]);
     lines.beam = animatedline('LineStyle', 'none', 'Marker', '.');
     lines.rod_b = animatedline('LineStyle', 'none', 'Marker', '+');
     % Draw rigid obstacle
     obs_pos = const.gap + const.Ly;
-    line(linspace(0.97, 1.03, 100), linspace(obs_pos, obs_pos, 100), ...
-        'LineStyle', 'none', 'Marker', 'd');
+    line(linspace(0.91, 1.09, 100), linspace(obs_pos, obs_pos, 100));
     
     % Beam's velocity
     subplot(3, 3, 3);
