72a6e2146769171a6b80b5e951b039604a85a5f1
Toad_Racer <gudak14@gmail.com>
2018-07-19 09:07:14 -0500

    Ensured that top node of rod will always be drawn

--- rod_animation_mask.m
+++ rod_animation_mask.m
@@ -1,6 +1,6 @@
 function mask = rod_animation_mask(const)
     mask = zeros(const.num_nodes, 1);
     spacing = int32(const.num_nodes/100);
-    for i = 1:spacing:const.num_nodes
+    for i = [1:spacing:const.num_nodes-1, const.num_nodes]
         mask(i) = 1;
     end
