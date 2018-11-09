88550c767988c535e790df523cbfbd14b5643fc2
Toad_Racer <gudak14@gmail.com>
2018-11-09 12:10:17 -0600

    Resized holistic plots to fit on one line

--- output-functions/prep_holistic.m
+++ output-functions/prep_holistic.m
@@ -1,21 +1,21 @@
 function lines = prep_holistic(initial_data, const, visibility)
     % Stress on Rod's top
     f1 = figure;
-    f1.OuterPosition = [50 50 500 250];
+    f1.OuterPosition = [50 400 250 250];
     title('Stress on Rod''s Top');
     axis([0, const.T, -inf, inf]);
     lines.sigma_dt = animatedline('Color', 'r');
 
     % Energy
     f2 = figure;
-    f2.OuterPosition = [50 300 500 250];
+    f2.OuterPosition = [300 400 250 250];
     title('Energy in System');
     axis([0, const.T, -inf, inf]);
     lines.energy = animatedline('Color', 'r');
 
     % Tip of rod
     f3 = figure;
-    f3.OuterPosition = [50 550 500 250];
+    f3.OuterPosition = [550 400 250 250];
     title('Tip of Rod');
     lines.tip = animatedline('Color', 'b');
 
