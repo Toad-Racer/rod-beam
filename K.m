d0c1b55a74a117df94416e1854c1e7bb2e79dd15
Toad_Racer <gudak14@gmail.com>
2018-12-14 15:11:22 -0600

    Dr. Ahn's new data

--- K.m
+++ K.m
@@ -2,10 +2,10 @@ function value = K(r)
     %{
      Computes the value of K for the current time-step.
 
-     @param    r    The scaler value of the natural variable r at the 
+     @param    r    The scaler value of the natural variable r at the
                     current time step.
 
      @return    value    The value of K at the current time-step.
     %}
     %value = (1/2 + 1/pi*atan(-10^3*r)); % formula taken from previous paper
-    value = 1000*(1/2 + (1/pi)*atan(-10^8*r));
+    value = 100*(1/2 + (1/pi)*atan(-10^8*r));
--- get_config.m
+++ get_config.m
@@ -19,16 +19,17 @@ function config = get_config(num_nodes, ht, num_steps)
     % beta=2500, beta=25
     % kappa_th = 0.85
     % kappa_th = 85
-    %run_simulation(1000,0.0001,10000)
-
+    % initial gap=0.002
+    % w^0= -x/1000 or -x/10
+    % run_simulation(1000,0.0001,10000)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Constants (to be consumed by get_constants.m)
     config.const = struct('alpha_d', 10^-6, 'alpha_b', 0, 'eta_d', 10, ...
-                          'kappa_th', 85, 'zeta_d', 1, 'Ed', 500, 'Eb', 150, 'G', 1000, ...
-                          'beta', 2500, 'rho', 1, 'gap', 0.002, 'Ly', 1, 'num_steps', num_steps, ...
+                          'kappa_th', 0.85, 'zeta_d', 1, 'Ed', 500, 'Eb', 150, 'G', 1000, ...
+                          'beta', 25, 'rho', 1, 'gap', 0.002, 'Ly', 1, 'num_steps', num_steps, ...
                           'num_nodes', num_nodes, 'ht', ht, 'T', num_steps*ht);
                            % Initial data (to be consumed by get_initial_data.m)
-    config.initial = struct('u', @(y) -0.001, 'ut', @(y) 0, 'w', @(x) -0.001*x, ...
+    config.initial = struct('u', @(y) -0.1, 'ut', @(y) 0, 'w', @(x) -0.1*x, ...
                             'wt', @(x) 0, 'theta', @(x) 0, 'theta_t', @(x) 0.0*x);
 
     %%% Output options
@@ -56,7 +57,7 @@ function config = get_config(num_nodes, ht, num_steps)
     config.log_fns = containers.Map(log_msgs, log_data);
 
     % For performance testing uncomment the following to suppress all output
-%     config.wait_for_input = false;
+    config.wait_for_input = false;
     config.log_fns = false;
     config.prep_plot_fn = @(x, y, z) NaN;
     config.plot_fn = @(x, y, z) NaN;
